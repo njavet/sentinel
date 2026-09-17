567aae529695d9f4249d6017e556cec490c285a0
Noe Javet <np.javet@gmail.com>
2026-09-17 11:25:02 +0200

    first crate dep

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("guess the number!");
+    println!("please input your guess.");
+    let mut guess = String::new();
+    io::stdin().read_line(&mut guess)
+        .expect("failed to read line");
+    println!("you guessed: {guess}");
 }
